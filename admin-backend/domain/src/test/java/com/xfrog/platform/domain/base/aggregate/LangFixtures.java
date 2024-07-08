f7377a9989a2d3b1ffe763ecdb617ebd94793893
xfrogcn <xfrogcn@163.com>
2024-07-08 17:01:36 +0800

    test: add lang fixtures

---
+++ admin-backend/domain/src/test/java/com/xfrog/platform/domain/base/aggregate/LangFixtures.java
@@ -0,0 +1,52 @@
+package com.xfrog.platform.domain.base.aggregate;
+
+public class LangFixtures {
+
+    public static final String DEFAULT_APPLICATION = "admin";
+
+    public static Lang.LangBuilder createDefaultLang() {
+        return Lang.builder()
+                .id(1L)
+                .application(DEFAULT_APPLICATION)
+                .code("zh-CN")
+                .localName("中文")
+                .name("中文")
+                .createdBy(1L)
+                .enabled(true);
+    }
+
+    public static LangCorpus.LangCorpusBuilder createDefaultCorpus() {
+        return LangCorpus.builder()
+                .id(1L)
+                .application(DEFAULT_APPLICATION)
+                .corpusCode("create-button-text")
+                .corpusGroup("test")
+                .corpusType("test")
+                .createdBy(1L)
+                .enabled(true);
+    }
+
+    public static LangLocal.LangLocalBuilder createDefaultLocal() {
+        return LangLocal.builder()
+                .id(1L)
+                .application(DEFAULT_APPLICATION)
+                .langCode("zh-CN")
+                .langCorpusCode("create-button-text")
+                .langCorpusId(1L)
+                .langId(1L)
+                .localValue("创建")
+                .createdBy(1L);
+    }
+
+    public static LangLocal.LangLocalBuilder createDefaultLocal(LangCorpus langCorpus, Lang lang) {
+        return LangLocal.builder()
+                .id(1L)
+                .application(langCorpus.getApplication())
+                .langCode(lang.getCode())
+                .langCorpusCode(langCorpus.getCorpusCode())
+                .langCorpusId(langCorpus.getId())
+                .langId(lang.getId())
+                .localValue("创建")
+                .createdBy(1L);
+    }
+}
